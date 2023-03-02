ddce4fba606d7011b25ed36036afe4c7906e826d
Miguel Quinteiros <quinteiros.m@gmail.com>
2023-03-02 19:36:10 +0100

    ex2 sans align

--- 02-intro-d3/src/index.js
+++ 02-intro-d3/src/index.js
@@ -3,9 +3,10 @@ import {select, selectAll} from "d3-selection";
 // C'est ici que vous allez écrire les premières lignes avec d3.js! | Non :p
 let width = "500";
 let height = "500";
+let dataset = [20, 5, 25, 8, 15];
+let circlesvg = select("body").append('div').attr('id', 'circleContainer').append("svg")
 
-let c1 = select("body")
-            .append("svg")
+let c1 = circlesvg
             .attr("width", width)
             .attr("height", height)
             .append('circle')
@@ -13,8 +14,7 @@ let c1 = select("body")
             .attr('cx', '100px')
             .attr('cy', '50px');
 
-let c2 = select("body")
-            .append("svg")
+let c2 = circlesvg
             .attr("width", width)
             .attr("height", height)
             .append('circle')
@@ -23,8 +23,7 @@ let c2 = select("body")
             .attr('cy', '150px')
             .attr("fill", 'green');
 
-let c3 = select("body")
-            .append("svg")
+let c3 = circlesvg
             .attr("width", width)
             .attr("height", height)
             .append('circle')
@@ -32,9 +31,31 @@ let c3 = select("body")
             .attr('cx', '300px')
             .attr('cy', '250px')
             .on('click', function(){
-                selectAll("svg")
-                .attr("margin", "auto");
+                selectAll('#circleContainer>svg')
+                .style('display','block')
+                .style('margin', 'auto')
             });;
 
 let text = selectAll("circle")
             .append("text");
+
+let dataContainer = select("body")
+                    .append('div')
+                    .attr("id", "dataContainer")
+                    .append("svg")
+                    .style('display','flex')
+                    .style('align-self','flex-end')
+                    .attr("width", width)
+                    .attr("height", '100px')
+
+let datasetShow = select("#dataContainer>svg")
+                .selectAll('rect')
+                .data("dataset")
+                .join("rect")
+                .attr('x', function(d,i){
+                    return i*20;
+                })
+                .attr('width', '20px')
+                .attr('height', function(d,i){
+                    return dataset[i]
+                })
